678a4264e1c6166006fcf5068f34a2edae0aa345
perlsmith <perlsmith@github.com>
2018-06-02 16:19:03 -0400

    after adding setreclimit and threading.stack_size

--- week2/merging_tables/by_learners/merging_tables.py
+++ week2/merging_tables/by_learners/merging_tables.py
@@ -1,21 +1,22 @@
 # python3
 
-import sys
 import pdb
+import sys, threading
+sys.setrecursionlimit(10**6) # max depth of recursion
+threading.stack_size(2**27)  # new thread will get stack of such size
 
-
-def getParent(table):
+def getParent(table, parents):
 	# int --> int
 	# find parent and compress path
 	if table != parents[table] :	# not the root
-		parents[table] = getParent( parents[table] )	# replace current parent
+		parents[table] = getParent( parents[table], parents )	# replace current parent
 	return parents[table]
 
-def merge(destination, source, lines, ans):
+def merge(destination, source, lines, ans, parents):
 	# (int, int, list, int ) --> int, and side effect is that lines and parents are manipulated
 	# given ans, it will return the updated ans - that is, lines of the destination topping existing ans will
 	# cause ans to be updated
-	realDestination, realSource = getParent(destination), getParent(source)
+	realDestination, realSource = getParent(destination, parents), getParent(source, parents)
 
 	if realDestination != realSource :
 
@@ -42,21 +43,23 @@ def merge(destination, source, lines, ans):
 		return ans
 		
 
-    
-if __name__ == '__main__':
+def main():
 	# sys.setrecursionlimit( 100000 )
-		
-	n, m = map(int, sys.stdin.readline().split())
-	lines = list(map(int, sys.stdin.readline().split()))
-	rank = [0] * n				# starter had 1 not 0
 	parents = list(range(0, n))	# to start with, each table is its own disjoint set. If parent[i] == i, then not a link :)
 	ans = max(lines)			# with each merge, check if A+B > ans and updated..
 	
 	# pdb.set_trace()
 	for i in range(m):
 		destination, source = map(int, sys.stdin.readline().split())
-		ans = merge(destination - 1, source - 1, lines, ans)
+		ans = merge(destination - 1, source - 1, lines, ans, parents)
 		print(   ans  )
+
+# if __name__ == '__main__':
+n, m = map(int, sys.stdin.readline().split())
+lines = list(map(int, sys.stdin.readline().split()))
+rank = [0] * n				# starter had 1 not 0
+
+threading.Thread(target=main).start()
 
 # n tables and m operations, each op is a merge and you have to report max size (of any table) after the op
 # not that that input is using 1 based indexing for the merge operations
